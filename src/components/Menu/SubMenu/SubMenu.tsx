d8f3fc20f5a8607105630360a2674cfa4856edb9
a-ghorbani <ghorbani59@gmail.com>
2024-12-04 16:26:39 +0100

    chore: add docs to submenu

--- src/components/Menu/SubMenu/SubMenu.tsx
+++ src/components/Menu/SubMenu/SubMenu.tsx
@@ -33,3 +33,37 @@ export const SubMenu: React.FC<SubMenuProps> = ({
     </PaperMenu>
   );
 };
+
+/**
+ * SubMenu component for nested menu items.
+ *
+ * Usage example:
+ * ```tsx
+ * <Menu.Item
+ *   label="Advanced"
+ *   submenu={[
+ *     <Menu.Item
+ *       key="1"
+ *       label="Option 1"
+ *     />,
+ *     // Nested submenu
+ *     <Menu.Item
+ *       label="More Options"
+ *       submenu={[
+ *         <Menu.Item key="2.1" label="Sub Option 1" />,
+ *         <Menu.Item key="2.2" label="Sub Option 2" />,
+ *       ]}
+ *     />,
+ *     <Menu.Item
+ *       key="3"
+ *       label="Option 3"
+ *     />,
+ *   ]}
+ * />
+ * ```
+ *
+ * Features:
+ * - Supports infinite nesting of submenus
+ * - Parent menu dims when submenu is open
+ * - Maintains consistent styling with parent menu
+ */
