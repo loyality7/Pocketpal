68ad1bf7423ee9069887a87c6fdac094d0580320
a-ghorbani <ghorbani59@gmail.com>
2024-12-09 17:16:04 +0100

    chore: update TextInput to support error messages

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,6 +2,7 @@ import React, {forwardRef} from 'react';
 import {View, TextInput as RNTextInput} from 'react-native';
 
 import {
+  HelperText,
   TextInput as PaperTextInput,
   TextInputProps as PaperTextInputProps,
 } from 'react-native-paper';
@@ -11,13 +12,16 @@ import {useTheme} from '../../hooks';
 import {createStyles} from './styles';
 
 export type TextInputProps = PaperTextInputProps & {
+  error?: boolean;
+  helperText?: string;
   showDivider?: boolean;
 };
 
 export const TextInput = forwardRef<RNTextInput, TextInputProps>(
-  ({style, showDivider = false, ...rest}, ref) => {
+  ({style, showDivider = false, error, helperText, ...rest}, ref) => {
     const theme = useTheme();
     const styles = createStyles(theme);
+
     return (
       <View style={styles.container}>
         <PaperTextInput
@@ -27,7 +31,13 @@ export const TextInput = forwardRef<RNTextInput, TextInputProps>(
           underlineColor={theme.colors.border}
           style={[styles.input, style]}
           placeholderTextColor={theme.colors.placeholder}
+          error={error}
         />
+        {helperText && (
+          <HelperText type={error ? 'error' : 'info'} visible={!!helperText}>
+            {helperText}
+          </HelperText>
+        )}
         {showDivider && <View style={styles.divider} />}
       </View>
     );
