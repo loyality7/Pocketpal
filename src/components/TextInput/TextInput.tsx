21859c86b4e8ae461712ae0a44fe281d5ea18d79
a-ghorbani <ghorbani59@gmail.com>
2024-12-09 14:33:59 +0100

    fix: correct ref forwarding for TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,33 +1,37 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {forwardRef} from 'react';
+import {View, TextInput as RNTextInput} from 'react-native';
 
-import {TextInput as PaperTextInput, TextInputProps} from 'react-native-paper';
+import {
+  TextInput as PaperTextInput,
+  TextInputProps as PaperTextInputProps,
+} from 'react-native-paper';
 
 import {useTheme} from '../../hooks';
 
 import {createStyles} from './styles';
 
-export type CustomTextInputProps = TextInputProps & {
+export type TextInputProps = PaperTextInputProps & {
   showDivider?: boolean;
 };
 
-export const TextInput: React.FC<CustomTextInputProps> = ({
-  style,
-  showDivider = false,
-  ...rest
-}) => {
-  const theme = useTheme();
-  const styles = createStyles(theme);
-  return (
-    <View style={styles.container}>
-      <PaperTextInput
-        {...rest}
-        dense
-        underlineColor={theme.colors.border}
-        style={[styles.input, style]}
-        placeholderTextColor={theme.colors.placeholder}
-      />
-      {showDivider && <View style={styles.divider} />}
-    </View>
-  );
-};
+export const TextInput = forwardRef<RNTextInput, TextInputProps>(
+  ({style, showDivider = false, ...rest}, ref) => {
+    const theme = useTheme();
+    const styles = createStyles(theme);
+    return (
+      <View style={styles.container}>
+        <PaperTextInput
+          ref={ref}
+          {...rest}
+          dense
+          underlineColor={theme.colors.border}
+          style={[styles.input, style]}
+          placeholderTextColor={theme.colors.placeholder}
+        />
+        {showDivider && <View style={styles.divider} />}
+      </View>
+    );
+  },
+);
+
+TextInput.displayName = 'TextInput';
